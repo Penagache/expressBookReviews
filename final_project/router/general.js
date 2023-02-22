c69a18a08ef4d397291ecbe129c34c4f664d4b77
Penagache <73881973+Penagache@users.noreply.github.com>
2023-02-22 13:30:21 +0100

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,17 +20,25 @@ public_users.post("/register", (req,res) => {
     return res.status(404).json({message: "Unable to register user."});
 });
 
+function getBookList(){
+    return new Promise((resolve,reject)=>{
+      resolve(books);
+    })
+  }
+  
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn])
  });
+  
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
